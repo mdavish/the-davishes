3630bee878bd6cf2e9c65ef14ee8c235fcae686f
Maxwell Davish <mdavish@yext.com>
2023-01-27 12:37:47 -0500

    working conversationId persistence

--- src/components/ChatBot.tsx
+++ src/components/ChatBot.tsx
@@ -4,6 +4,7 @@ import { IoCaretDownOutline } from "react-icons/io5";
 import cx from "classnames";
 import { IoChatbubblesSharp } from "react-icons/io5";
 import { getRuntime } from "@yext/pages/util"
+import { z } from "zod";
 
 interface IFrameParams {
   conversationId?: string;
@@ -12,37 +13,52 @@ interface IFrameParams {
   visitorId?: string;
 }
 
+const EventDataSchema = z.object({
+  data: z.object({
+    id: z.string(),
+  }),
+  eventType: z.string(),
+  source: z.string(),
+})
+
+const baseUrl = "https://yext-chatbot-prod.up.railway.app/bots";
+
 export function ChatBot({ configId }: { configId: string }) {
 
   const runtime = getRuntime();
   const [showChat, setShowChat] = useState(false);
   const iframeRef = useRef<HTMLIFrameElement>(null);
-
-  const iframeParams: IFrameParams = {
-    // pageUrl: window.location.href,
-  }
-
-  // When the page loads, check to see if there is a conversationId in session storage
-  // If there is, pass it as a URL param to the iframe
+  const [iframeParams, setIframeParams] = useState<IFrameParams>({});
 
   useEffect(() => {
     if (runtime.name === "browser") {
       const pageUrl = window.location.href;
       const referrerPageUrl = document.referrer;
-      const conversationId = sessionStorage.getItem("conversationId");
-      if (conversationId) {
-        iframeParams.conversationId = conversationId;
-      }
-      iframeParams.pageUrl = pageUrl;
-      iframeParams.referrerPageUrl = referrerPageUrl;
+      let conversationId: string | null = sessionStorage.getItem("conversationId");
+      conversationId = conversationId === "undefined" ? null : conversationId;
+      const visitorId = sessionStorage.getItem("visitorId");
+      setIframeParams({
+        pageUrl,
+        referrerPageUrl,
+        ...(conversationId && { conversationId }),
+        ...(visitorId && { visitorId })
+      });
     }
   }, []);
 
   useEffect(() => {
     if (runtime.name === "browser") {
       const handleMessage = (event: MessageEvent) => {
-        if (event.data && event.data.conversationId) {
-          sessionStorage.setItem("conversationId", event.data.conversationId);
+
+        let parsedData;
+        try {
+          parsedData = EventDataSchema.parse(event.data)
+        } catch (e) {
+          return;
+        }
+
+        if (parsedData.eventType === "createNewConversation") {
+          sessionStorage.setItem("conversationId", parsedData.data.id);
         }
       };
 
@@ -53,8 +69,8 @@ export function ChatBot({ configId }: { configId: string }) {
     }
   }, [iframeRef.current]);
 
-  const iFrameUrl = new URL(`https://yext-chatbot-prod.up.railway.app/bots/${configId}`);
-  const params = new URLSearchParams(JSON.stringify(iframeParams));
+  const iFrameUrl = new URL(`${baseUrl}/${configId}`);
+  const params = new URLSearchParams(Object.entries(iframeParams).filter(([_, value]) => value !== null));
   iFrameUrl.search = params.toString();
 
   return (
@@ -64,7 +80,7 @@ export function ChatBot({ configId }: { configId: string }) {
       <Transition className="fixed right-4 bottom-24 lg:bottom-40 lg:right-10 w-80 lg:w-96 h-3/4 lg:h-2/3 xl:1/2  bg-white rounded-xl shadow-xl overflow-hidden" as="div" enter="transition-opacity duration-300" enterFrom="opacity-0" enterTo="opacity-100" leave="transition-opacity duration-300" leaveFrom="opacity-100" leaveTo="opacity-0" show={showChat}>
         <iframe
           ref={iframeRef}
-          className="w-full h-full" src={`https://yext-chatbot-prod.up.railway.app/bots/${configId}`} />
+          className="w-full h-full" src={iFrameUrl.toString()} />
       </Transition>
       <button onClick={() => setShowChat(!showChat)} className="border border-white shadow-xl hover:shadow-2xl bottom-10 right-10 w-12 h-12 lg:w-20 lg:h-20 rounded-full bg-gradient-to-br from-green-800 to-green-1100 hover:-translate-y-2 transition-all duration-150">
         <div className="flex flex-col justify-center items-center h-full w-full text-white">
